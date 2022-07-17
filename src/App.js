789cd6e4a4d60c89cc86c06447476ad8ca5d7133
mokshi1411 <mokshi1411@gmail.com>
2022-07-18 01:31:15 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
-import './App.css';
-
+import React,{useState} from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Routes";
 function App() {
+  const [darktheme,setdarktheme]=useState(false);
   return (
-    <div className="App">
-      
+    
+    <div className={darktheme ? "dark":" "}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen" >
+      MOMO
+      </div> 
     </div>
   );
 }
