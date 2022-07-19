02210ee4c65fbfcbe6343e7ddb9581747154be77
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2022-07-19 17:31:11 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Routes from "./components/Routes";
 function App() {
-  const [darktheme,setdarktheme]=useState(false);
+  const [darktheme, setdarktheme] = useState(true);
+
   return (
-    
-    <div className={darktheme ? "dark":" "}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen" >
-      MOMO
-      </div> 
+    <div className={darktheme ? "dark" : " "}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darktheme} setDarkTheme={setdarktheme} />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }
